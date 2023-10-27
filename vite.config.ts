93ae50b762a2b0aeb7354f0d0da4bde95f2fcaf0
Felix <felix4hubert@gmail.com>
2023-10-27 16:09:00 -0400

    try update repo to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import viteTsconfigPaths from "vite-tsconfig-paths";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "",
-  plugins: [react(), viteTsconfigPaths()],
-  server: {
-    open: true,
-    port: 3000,
-  },
+  plugins: [react()],
 });
